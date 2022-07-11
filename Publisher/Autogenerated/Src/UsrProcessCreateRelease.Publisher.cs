50ee11308632b659df7d672969d68b62b9769db0
Владислав <rogatihvlad@gmail.com>
2022-07-11 16:14:22 +0300

    Реализовано действие добавления записей в деталь с автоматическим обновлением данных

---
+++ Publisher/Autogenerated/Src/UsrProcessCreateRelease.Publisher.cs
@@ -0,0 +1,48 @@
+﻿namespace Terrasoft.Core.Process
+{
+
+	using System;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using System.Drawing;
+	using System.Globalization;
+	using System.Text;
+	using Terrasoft.Common;
+	using Terrasoft.Core;
+	using Terrasoft.Core.Configuration;
+	using Terrasoft.Core.DB;
+	using Terrasoft.Core.Entities;
+	using Terrasoft.Core.Process;
+	using Terrasoft.Core.Process.Configuration;
+
+	#region Class: UsrProcessCreateReleaseMethodsWrapper
+
+	/// <exclude/>
+	public class UsrProcessCreateReleaseMethodsWrapper : ProcessModel
+	{
+
+		public UsrProcessCreateReleaseMethodsWrapper(Process process)
+			: base(process) {
+			AddScriptTaskMethod("ScriptTask1Execute", ScriptTask1Execute);
+		}
+
+		#region Methods: Private
+
+		private bool ScriptTask1Execute(ProcessExecutingContext context) {
+			// Формирование текста сообщения.
+			string messageText = Get<Guid>("ProcessSchemaParameterIdPublish").ToString();
+			// Присвоение сообщению имени.
+			string sender = "NewRelease";
+			// Публикация сообщения по WebSocket.
+			Terrasoft.Configuration.MsgChannelUtilities.PostMessage(UserConnection, sender, messageText);
+			return true;
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
+
